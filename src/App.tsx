98cb19927efac4bd76178263feda94c371a99f35
FriedAxons <friedaxons@proton.me>
2024-12-17 12:32:09 -0600

    Import react router and svg and component changes
    Added a fill to the dark mode moon icon to be
    filled with white. Imported react-router for
    implementing routes to the Home and CountryDetails
    pages. And changes to the Header for the dark
    and light mode changes.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,18 @@
-const App = () => {
-  return <p>Hi :)</p>;
+import { BrowserRouter as Router, Routes, Route } from "react-router";
+import Header from "./components/Header";
+// import Home from "./pages/Home";
+// import CountryDetails from "./pages/CountryDetails";
+
+const App = (): JSX.Element => {
+  return (
+    <Router>
+      <div className="dark:bg-verydarkblue bg-lverylightgray">
+        <Header />
+        {/* <Routes>
+        </Routes> */}
+      </div>
+    </Router>
+  );
 };
 
 export default App;
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,7 @@ import ThemeToggle from "./ThemeToggle";
 
 const Header = (): JSX.Element => {
   return (
-    <header>
+    <header className="dark:bg-darkblue bg-white">
       <h1>Where in the world?</h1>
       <ThemeToggle />
     </header>

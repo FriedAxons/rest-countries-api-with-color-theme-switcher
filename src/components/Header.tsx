ac8dce9b4164b6157a670b42641cb0acf04ddbf0
FriedAxons <friedaxons@proton.me>
2024-12-17 13:45:17 -0600

    Header and ThemeToggle changes
    Wrapped ToggleTheme with a div so that the custom
    padding applies correctly, pushing in the icon and
    text. In index.css, the custom padding was changed
    to apply a universal x padding. One was added for
    the mobile version as well.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,9 +2,11 @@ import ThemeToggle from "./ThemeToggle";
 
 const Header = (): JSX.Element => {
   return (
-    <header className="dark:bg-darkblue bg-white">
+    <header className="flex justify-between items-center dark:bg-darkblue bg-white pt-8 lg:pt-5 pb-8 lg:pb-5 mx-p lg:x-p">
       <h1>Where in the world?</h1>
-      <ThemeToggle />
+      <div className="flex items-center">
+        <ThemeToggle />
+      </div>
     </header>
   );
 };
--- src/components/ThemeToggle.tsx
+++ src/components/ThemeToggle.tsx
@@ -20,20 +20,22 @@ const ThemeToggle = (): JSX.Element => {
   };
 
   return (
-    <button onClick={toggleTheme}>
+    <button onClick={toggleTheme} className="flex items-center space-x-2">
       {/* Display appropriate SVG icon based on the theme */}
       {theme === "dark" ? (
         <img
           src="/rest-countries-api-with-color-theme-switcher/icons/moon.svg"
           alt="Moon Icon"
+          className="w-4 h-4"
         />
       ) : (
         <img
           src="/rest-countries-api-with-color-theme-switcher/icons/moon-outline.svg"
           alt="Moon Outline Icon"
+          className="w-4 h-4"
         />
       )}
-      Dark Mode
+      <span>Dark Mode</span>
     </button>
   );
 };

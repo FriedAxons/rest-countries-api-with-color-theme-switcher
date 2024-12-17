55eb69207fd9f117c0a88b3938625f86678050ec
FriedAxons <friedaxons@proton.me>
2024-12-16 22:49:55 -0600

    Adding comments to the colors in tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,12 +9,12 @@ export default {
     extend: {
       colors: {
         // Custom colors for light/dark mode
-        darkblue: "hsl(209, 23%, 22%)",
-        verydarkblue: "hsl(207, 26%, 17%)",
-        lverydarkblue: "hsl(200, 15%, 8%)",
-        ldarkgray: "hsl(0, 0%, 52%)",
-        lverylightgray: "hsl(0, 0%, 98%)",
-        white: "hsl(0, 0%, 100%)",
+        darkblue: "hsl(209, 23%, 22%)", // (Dark Mode Elements)
+        verydarkblue: "hsl(207, 26%, 17%)", // (Dark Mode Background)
+        lverydarkblue: "hsl(200, 15%, 8%)", // (Light Mode Text)
+        ldarkgray: "hsl(0, 0%, 52%)", // (Light Mode Input)
+        lverylightgray: "hsl(0, 0%, 98%)", // (Light Mode Background)
+        white: "hsl(0, 0%, 100%)", // (Dark Mode Text & Light Mode Elements)
       },
       fontFamily: {
         "nunito-sans": ["Nunito Sans", "sans-serif"],

2a513946849c8774e284aa935c37a75a41f3e91b
FriedAxons <friedaxons@proton.me>
2024-12-16 19:08:02 -0600

    Add custom colors and font

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,15 @@ export default {
     extend: {
       colors: {
         // Custom colors for light/dark mode
+        darkblue: "hsl(209, 23%, 22%)",
+        verydarkblue: "hsl(207, 26%, 17%)",
+        lverydarkblue: "hsl(200, 15%, 8%)",
+        ldarkgray: "hsl(0, 0%, 52%)",
+        lverylightgray: "hsl(0, 0%, 98%)",
+        white: "hsl(0, 0%, 100%)",
+      },
+      fontFamily: {
+        "nunito-sans": ["Nunito Sans", "sans-serif"],
       },
     },
   },

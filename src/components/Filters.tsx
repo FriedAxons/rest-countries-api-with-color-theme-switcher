031ddde0d0bf30f5612f0d3ac3c16c1ade40e1ab
FriedAxons <friedaxons@proton.me>
2024-12-18 15:11:18 -0600

    Style Filters component

--- src/components/Filters.tsx
+++ src/components/Filters.tsx
@@ -22,7 +22,7 @@ const Filters = ({
     <svg
       xmlns="http://www.w3.org/2000/svg"
       viewBox="0 0 512 512"
-      className="w-5 h-5 fill-ldarkgray dark:fill-white" // Use fill to control color in light/dark mode
+      className="w-4 h-4 fill-ldarkgray dark:fill-white" // Use fill to control color in light/dark mode
     >
       <path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z" />
     </svg>
@@ -33,7 +33,7 @@ const Filters = ({
     <svg
       xmlns="http://www.w3.org/2000/svg"
       viewBox="0 0 448 512"
-      className="w-4 h-4 fill-lverydarkblue dark:fill-white" // Use fill to control color in light/dark mode
+      className="w-3 h-4 fill-lverydarkblue dark:fill-white" // Use fill to control color in light/dark mode
     >
       <path d="M201.4 137.4c12.5-12.5 32.8-12.5 45.3 0l160 160c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L224 205.3 86.6 342.6c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3l160-160z" />
     </svg>
@@ -59,7 +59,7 @@ const Filters = ({
           value={search}
           onChange={(e) => setSearch(e.target.value)}
           placeholder="Search for a country..."
-          className="w-full dark:bg-darkblue bg-white placeholder:text-sm placeholder-ldarkgray dark:placeholder-white dark:text-white text-lverydarkblue font-light py-3 pl-16 rounded-md shadow-md"
+          className="w-full dark:bg-darkblue bg-white placeholder:text-sm placeholder-ldarkgray dark:placeholder-white dark:text-white text-lverydarkblue dark:font-light font-normal py-3 pl-16 rounded-md shadow-md"
         />
         <div className="absolute left-6 top-1/2 transform -translate-y-1/2">
           <MagnifyingGlassIcon />
@@ -70,7 +70,7 @@ const Filters = ({
       <div className="relative">
         <button
           onClick={toggleDropdown}
-          className="flex items-center dark:bg-darkblue bg-white text-lverydarkblue dark:text-white py-3 rounded-md shadow-md"
+          className="flex items-center dark:bg-darkblue bg-white text-lverydarkblue dark:text-white dark:font-light font-medium text-sm py-3 rounded-md shadow-md"
           aria-haspopup="listbox"
           aria-expanded={showDropdown ? "true" : "false"}
         >
